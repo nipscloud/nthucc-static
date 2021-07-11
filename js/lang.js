3fb870dc38c47aa49f629a4cd91e010048363ab6
nipscloud <61387219+nipscloud@users.noreply.github.com>
2021-07-11 18:33:21 +0800

    no debug log

--- js/lang.js
+++ js/lang.js
@@ -5,10 +5,8 @@ langCss.setAttribute("type", "text/css");
 if(navigator.language.startsWith("en")){
    /* hide chinese div when browser language is English */
    langCss.setAttribute("href", "https://cdn.jsdelivr.net/gh/nipscloud/nthucc-img@main/css/package-en.css")
-   console.log(navigator.language, "use Engish env");
 }else{
    langCss.setAttribute("href", "https://cdn.jsdelivr.net/gh/nipscloud/nthucc-img@main/css/package-zh.css");
-   console.log(navigator.language, "use Chinese env");
 }
 
 document.head.appendChild(langCss);
