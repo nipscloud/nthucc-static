6a6eea9018c46c2c914284c8003ff18b0d57c613
phlinhng <phlinhng@outlook.com>
2021-07-12 14:22:55 +0800

    change directory structure

--- js/lang.js
+++ js/lang.js
@@ -4,9 +4,9 @@ langCss.setAttribute("type", "text/css");
 
 if(navigator.language.startsWith("en")){
    /* hide chinese div when browser language is English */
-   langCss.setAttribute("href", "https://cdn.jsdelivr.net/gh/nipscloud/nthucc-img@main/css/package-en.css")
+   langCss.setAttribute("href", "https://cdn.jsdelivr.net/gh/nipscloud/nthucc-static@main/css/package-en.css")
 }else{
-   langCss.setAttribute("href", "https://cdn.jsdelivr.net/gh/nipscloud/nthucc-img@main/css/package-zh.css");
+   langCss.setAttribute("href", "https://cdn.jsdelivr.net/gh/nipscloud/nthucc-static@main/css/package-zh.css");
 }
 
 document.head.appendChild(langCss);
